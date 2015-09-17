448df0c250fd7207344683f8db2938dba9845296
Joseph Utecht <thenmal@gmail.com>
2015-09-17 10:41:45 -0500

    added comments

--- update_sheet.py
+++ update_sheet.py
@@ -4,23 +4,21 @@ import webbrowser
 import httplib2
 from oauth2client import client
 
+# Authenticate with Google Services
 flow = client.flow_from_clientsecrets(
         'secret.json',
         scope = 'https://spreadsheets.google.com/feeds',
         redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-
 auth_uri = flow.step1_get_authorize_url()
 webbrowser.open(auth_uri)
-
-
 auth_code = raw_input('Enter the auth code: ')
-
 credentials = flow.step2_exchange(auth_code)
 http_auth = credentials.authorize(httplib2.Http())
 
-
+# Open Google Sheet
 gc = gspread.authorize(credentials)
 sheet = gc.open("OOSTT Sheet Test").sheet1
 
+# Load OOSTT Ontology
 g = Graph()
 g.parse('https://raw.githubusercontent.com/OOSTT/OOSTT/master/oostt.owl', format='xml')
