ff3704dfd36f20a4e40496336009a40b215e0dd9
unknown <2986165@DADM432.ad.uams.edu>
2015-10-21 15:27:25 -0500

    Matches nodes w/columns. Still need to make code search for existing term in sheet before it writes anything.

--- update_sheet.py
+++ update_sheet.py
@@ -8,9 +8,9 @@ from oauth2client import client
 
 # Authenticate with Google Services
 flow = client.flow_from_clientsecrets(
-   'secret.json',
-   scope='https://spreadsheets.google.com/feeds',
-   redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+    'secret.json',
+    scope='https://spreadsheets.google.com/feeds',
+    redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 auth_uri = flow.step1_get_authorize_url()
 webbrowser.open(auth_uri)
 auth_code = raw_input('Enter the auth code: ')
@@ -23,23 +23,23 @@ sheet = gc.open("OOSTT Sheet Test").sheet1
 
 # SPARQL node : column heading
 nodes = {
-    'term' : 'Term',
-    'genus' : 'Definition (genus differentia)',
-    'userdef' : 'rdfs:comment (user-centered definitions)'
+    'term': 'Term',
+    'genus': 'Definition (genus differentia)',
+    'userdef': 'rdfs:comment (user-centered definitions)'
 }
 
 # Only uncomment next 2 lines if creating new sheet:
 # sheet.resize(1)
 # sheet.append_row(nodes.values())
-
 sheet_row = sheet.row_count  # number rows before writing to sheet
+
 # Load OOSTT Ontology
 g = Graph()
 g.parse('https://raw.githubusercontent.com/OOSTT/OOSTT/master/oostt.owl', format='xml')
 
 # Run SPARQL query & write results to appropriate columns
 for row in g.query(
-       """PREFIX obo: <http://purl.obolibrary.org/obo/>
+        """PREFIX obo: <http://purl.obolibrary.org/obo/>
       SELECT ?term ?genus ?userdef
       WHERE {
         ?class rdf:type owl:Class .
@@ -48,7 +48,7 @@ for row in g.query(
         OPTIONAL {?class obo:OOSTT_00000030 ?userdef .}
       }
        """):
-   sheet_row += 1
-   sheet.add_rows(1)
-   for node in nodes:
-       sheet.update_cell(sheet_row, sheet.find(nodes[node]).col, row[node])
+    sheet_row += 1
+    sheet.add_rows(1)
+    for node in nodes:
+        sheet.update_cell(sheet_row, sheet.find(nodes[node]).col, row[node])
