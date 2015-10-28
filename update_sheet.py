bf868f5f1f33621e29bea2e138f20ee5821f9812
unknown <2986165@DADM432.ad.uams.edu>
2015-10-28 08:40:54 -0500

    Adds definitions/descriptions to same row as matching term

--- update_sheet.py
+++ update_sheet.py
@@ -2,15 +2,16 @@ from rdflib import Graph
 from rdflib import Namespace
 from rdflib.namespace import RDF, RDFS, OWL, XSD
 import gspread
+from gspread.exceptions import CellNotFound
 import webbrowser
 import httplib2
 from oauth2client import client
 
 # Authenticate with Google Services
 flow = client.flow_from_clientsecrets(
-    'secret.json',
-    scope='https://spreadsheets.google.com/feeds',
-    redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+   'secret.json',
+   scope='https://spreadsheets.google.com/feeds',
+   redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 auth_uri = flow.step1_get_authorize_url()
 webbrowser.open(auth_uri)
 auth_code = raw_input('Enter the auth code: ')
@@ -23,9 +24,9 @@ sheet = gc.open("OOSTT Sheet Test").sheet1
 
 # SPARQL node : column heading
 nodes = {
-    'term': 'Term',
-    'genus': 'Definition (genus differentia)',
-    'userdef': 'rdfs:comment (user-centered definitions)'
+   'term': 'Term',
+   'genus': 'Definition (genus differentia)',
+   'userdef': 'rdfs:comment (user-centered definitions)'
 }
 
 # Only uncomment next 2 lines if creating new sheet:
@@ -39,16 +40,19 @@ g.parse('https://raw.githubusercontent.com/OOSTT/OOSTT/master/oostt.owl', format
 
 # Run SPARQL query & write results to appropriate columns
 for row in g.query(
-        """PREFIX obo: <http://purl.obolibrary.org/obo/>
-      SELECT ?term ?genus ?userdef
-      WHERE {
-        ?class rdf:type owl:Class .
-        ?class rdfs:label ?term .
-        OPTIONAL {?class obo:IAO_0000115 ?genus .}
-        OPTIONAL {?class obo:OOSTT_00000030 ?userdef .}
-      }
-       """):
-    sheet_row += 1
-    sheet.add_rows(1)
-    for node in nodes:
-        sheet.update_cell(sheet_row, sheet.find(nodes[node]).col, row[node])
+       """PREFIX obo: <http://purl.obolibrary.org/obo/>
+    SELECT ?term ?genus ?userdef
+    WHERE {
+      ?class rdf:type owl:Class .
+      ?class rdfs:label ?term .
+      OPTIONAL {?class obo:IAO_0000115 ?genus .}
+      OPTIONAL {?class obo:OOSTT_00000030 ?userdef .}
+    }
+     """):
+   try:
+       sheet_row = sheet.find(str(row['term'])).row
+   except CellNotFound:
+       sheet_row = sheet.row_count + 1
+       sheet.add_rows(1)
+   for node in nodes:
+       sheet.update_cell(sheet_row, sheet.find(nodes[node]).col, row[node])
